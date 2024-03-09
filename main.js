3bf4a01524a8be38786ae0d736db629e86633d1f
Yuli <edyuarca_15@hotmail.com>
2024-03-08 22:27:53 -0300

    funcionalidad btn agregar operación

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+//Contendor operaciones vacío a agregar nueva operación
+const btnNuevaOperacion = document.getElementById("btnAgregarOperacion");
+const containerNuevaOp = document.getElementById("containerNuevaOp");
+const containerVacio = document.getElementById("containerVacio");
+const contenedorBalances = document.getElementById("contenedorBalances");
+
+const vercontainerNuevaOp = () => {
+  containerNuevaOp.style.display = "block";
+  containerVacio.style.display = "none";
+  contenedorBalances.style.display = "none";
+};
+btnNuevaOperacion.addEventListener(`click`, vercontainerNuevaOp);
+
+//Agregar tabla
+
 const categorias = document.getElementById("categorias");
 const datos = [];
 const evaluarLocalStorage = () => {
