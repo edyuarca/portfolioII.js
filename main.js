c5f154c2ff9210340d0743570af99f0c2fd0b816
Yuli <edyuarca_15@hotmail.com>
2024-03-18 23:46:12 -0300

    se agrego funcionalidad envio de formulario y generar tabla en js

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const containerVacio = document.getElementById("containerVacio");
 const contenedorBalances = document.getElementById("contenedorBalances");
 
 const vercontainerNuevaOp = () => {
-  containerNuevaOp.style.display = "block";
-  containerVacio.style.display = "none";
-  contenedorBalances.style.display = "none";
+  containerNuevaOp.classList.remove("hidden");
+  containerVacio.classList.add("hidden");
+  contenedorBalances.classList.add("hidden");
 };
 btnNuevaOperacion.addEventListener(`click`, vercontainerNuevaOp);
 
@@ -21,9 +21,22 @@ const monto = document.getElementById("monto");
 const tipo = document.getElementById("tipo");
 const categoria = document.getElementById("categoria");
 const fecha = document.getElementById("fecha");
+const operaciones = document.getElementById("operaciones");
+const tablaOperaciones = document.getElementById("tablaOperaciones");
 
 let datos = [];
-console.log(datos);
+
+const evaluarLocalStorage = () => {
+  if (localStorage.getItem("operaciones") !== null) {
+    const operacionesGuardadas = JSON.parse(
+      localStorage.getItem("operaciones")
+    );
+    return operacionesGuardadas;
+  } else {
+    localStorage.setItem("operaciones", JSON.stringify(datos));
+  }
+  generarTabla();
+};
 
 formNuevaOperacion.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -36,25 +49,18 @@ formNuevaOperacion.addEventListener("submit", (e) => {
     categoria: categoria.value,
     fecha: fecha.value,
   };
-
+  console.log(categoria.value);
   console.log("Datos de la nueva operación:", OperacionNueva);
-
+  datos = evaluarLocalStorage() || [];
   datos.push(OperacionNueva);
   localStorage.setItem("operaciones", JSON.stringify(datos));
 
-  formNuevaOperacion.reset();
-});
+  formNuevaOperacion.classList.add("hidden");
+  contenedorBalances.classList.remove("hidden");
+  tablaOperaciones.classList.remove("hidden");
 
-const evaluarLocalStorage = () => {
-  if (localStorage.getItem("operaciones") !== null) {
-    localStorage.getItem("operaciones", JSON.parse(datos));
-    console.log("operaciones traidas del local:", operacionesGuardadas);
-    return operacionesGuardadas;
-  } else {
-    localStorage.setItem("operaciones", JSON.stringify(datos));
-  }
   generarTabla();
-};
+});
 
 //boton cancelar envío
 
@@ -65,3 +71,27 @@ const pagPricinpal = () => {
 };
 
 btnCancelarOpTabla.addEventListener("click", pagPricinpal);
+
+//creación de tabla
+console.log(operaciones);
+const generarTabla = () => {
+  operaciones.innerHTML = "";
+  console.log(evaluarLocalStorage());
+  if (evaluarLocalStorage()) {
+    console.log("hola");
+    for (let operacion of evaluarLocalStorage()) {
+      console.log(operacion.categoria);
+      operaciones.innerHTML += `
+        <div>
+          <span>${operacion.descripcion}</span>
+          <span>${operacion.categoria}</span>
+          <span>${operacion.fecha}</span>
+          <span>${operacion.monto}</span>
+
+        </div>
+        `;
+    }
+  }
+  console.log("Tabla generada correctamente");
+};
+generarTabla();
