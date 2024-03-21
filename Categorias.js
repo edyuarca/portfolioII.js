6901ec9dcaf472bceda2637be28ee97b9b6a010f
cinthia sosa <cinthia_ramlah@hotmail.com>
2024-03-21 00:00:27 -0300

    se agrego funcion de cancelar

--- Categorias.js
+++ Categorias.js
@@ -1,13 +1,13 @@
-// Obtener todos los botones de edición de categoría
+// Obtener todos los botones de editar de categoría
 const editarCategoriaBtns = document.querySelectorAll('.editar-categoria-btn');
 
 // Agregar un evento de clic a cada botón de edición de categoría
 editarCategoriaBtns.forEach(btn => {
     btn.addEventListener('click', () => {
-        // Obtener el ID único del div de edición de categoría correspondiente
+        
         const categoria = btn.getAttribute('data-categoria');
         const editarCategoriaDiv = document.getElementById(`editarCategoriaDiv-${categoria}`);
-        const categoriasDiv = document.getElementById('box2'); // Obtener el div de categorías
+        const categoriasDiv = document.getElementById('box2'); //  div de categorías
 
         // Ocultar el div de categorías
         categoriasDiv.style.display = 'none';
@@ -20,16 +20,16 @@ editarCategoriaBtns.forEach(btn => {
 // Obtener todos los botones del menú
 const menuBtns = document.querySelectorAll('.opcion');
 
-// Agregar un evento de clic a cada botón del menú
+
 menuBtns.forEach(btn => {
     btn.addEventListener('click', () => {
-        // Obtener el ID del div correspondiente a este botón
+        
         const targetDivId = btn.getAttribute('data-target');
         
-        // Ocultar todos los divs excepto el div del objetivo
+        // Ocultar todos los divs excepto el div principal
         document.querySelectorAll('[data-box]').forEach(div => {
             if (div.id === targetDivId) {
-                div.style.display = 'block'; // Mostrar el div objetivo
+                div.style.display = 'block'; // Mostrar el div 
             } else {
                 div.style.display = 'none'; // Ocultar otros divs
             }
@@ -37,12 +37,12 @@ menuBtns.forEach(btn => {
     });
 });
 
-// Obtener el botón de cancelar
+// Botón de cancelar
 const cancelarCategoriaBtn = document.getElementById('cancelar-categoria-boton');
 
-// Agregar un evento de clic al botón de cancelar
+
 cancelarCategoriaBtn.addEventListener('click', () => {
-    // Ocultar el div de edición de categoría
+    // Ocultar el div de editar de categoría
     document.querySelectorAll('[id^="editarCategoriaDiv"]').forEach(div => {
         div.style.display = 'none';
     });
