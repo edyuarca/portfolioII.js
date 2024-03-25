a6f59900a6b4d0daf9be17a8f465409f0dcdea41
cinthia sosa <cinthia_ramlah@hotmail.com>
2024-03-24 21:37:43 -0300

    se agrego funcion en main.js ocultar/mostrar filtros

--- main.js
+++ main.js
@@ -26,3 +26,23 @@ const evaluarLocalStorage = () => {
     localStorage.setItem("operaciones", JSON.stringify(datos));
   }
 };
+
+//filtros ocultar/mostrar
+//FILTROS
+
+document.addEventListener("DOMContentLoaded", function() {
+    var toggleFiltros = document.getElementById("toggle-filtros");
+    var filtros = document.getElementById("filtros");
+
+    toggleFiltros.addEventListener("click", function(event) {
+        event.preventDefault(); 
+
+        if (filtros.style.display === "none") {
+            filtros.style.display = "block";
+            toggleFiltros.textContent = "Ocultar filtros";
+        } else {
+            filtros.style.display = "none";
+            toggleFiltros.textContent = "Mostrar filtros";
+        }
+    });
+});
