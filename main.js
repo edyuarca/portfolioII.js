eca5dcfbd8cc1daf79c7e7563632233ec3a74540
cinthia sosa <cinthia_ramlah@hotmail.com>
2024-03-25 00:01:37 -0300

    se agrega funcion a nueva operacion main.js

--- main.js
+++ main.js
@@ -19,3 +19,33 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+//Contendor operaciones vacío a agregar nueva operación
+const btnNuevaOperacion = document.getElementById("btnAgregarOperacion");
+const contNuevaOp = document.getElementById("contNuevaOp");
+const contVacio = document.getElementById("contVacio");
+const contBalancesFiltros = document.getElementById("contBalancesFiltros");
+
+const vercontainerNuevaOp = () => {
+  contNuevaOp.classList.remove("hidden");
+  contVacio.style.display = "none";
+  contBalancesFiltros.style.display = "none";
+};
+btnNuevaOperacion.addEventListener(`click`, vercontainerNuevaOp);
+
+//Agregar tabla
+
+const nuevaOperacionForm = document.getElementById("nuevaOperacionForm");
+
+// const categorias = document.getElementById("categorias");
+// const datos = [];
+
+const evaluarLocalStorage = () => {
+  if (localStorage.getItem("operaciones") !== null) {
+    operacionesGuardadas = JSON.parse(localStorage.getItem("operaciones"));
+    return operacionesGuardadas;
+  } else {
+    localStorage.setItem("operaciones", JSON.stringify(datos));
+  }
+};
+
