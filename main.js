2bf6aca68e41f758f3d87cbbfeefcf01282649cc
cinthia sosa <cinthia_ramlah@hotmail.com>
2024-03-24 21:34:46 -0300

    se agrego funcion en main.js ocultar filtros

---
+++ main.js
@@ -0,0 +1,21 @@
+//FILTROS
+
+document.addEventListener("DOMContentLoaded", function() {
+    var toggleFiltros = document.getElementById("toggle-filtros");
+    var filtros = document.getElementById("filtros");
+
+    toggleFiltros.addEventListener("click", function(event) {
+        event.preventDefault(); // Evita que el enlace recargue la página
+
+        // Verifica el estado actual de los filtros
+        if (filtros.style.display === "none") {
+            // Si están ocultos, muestra los filtros
+            filtros.style.display = "block";
+            toggleFiltros.textContent = "Ocultar filtros";
+        } else {
+            // Si están visibles, oculta los filtros
+            filtros.style.display = "none";
+            toggleFiltros.textContent = "Mostrar filtros";
+        }
+    });
+});
