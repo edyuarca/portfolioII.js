c779b6ab3094b9e5fe2e8fda326d700bf43d954f
cinthia sosa <cinthia_ramlah@hotmail.com>
2024-03-24 21:38:50 -0300

    se corrigio orden en main.js

--- main.js
+++ main.js
@@ -30,19 +30,19 @@ const evaluarLocalStorage = () => {
 //filtros ocultar/mostrar
 //FILTROS
 
-document.addEventListener("DOMContentLoaded", function() {
-    var toggleFiltros = document.getElementById("toggle-filtros");
-    var filtros = document.getElementById("filtros");
+document.addEventListener("DOMContentLoaded", function () {
+  var toggleFiltros = document.getElementById("toggle-filtros");
+  var filtros = document.getElementById("filtros");
 
-    toggleFiltros.addEventListener("click", function(event) {
-        event.preventDefault(); 
+  toggleFiltros.addEventListener("click", function (event) {
+    event.preventDefault();
 
-        if (filtros.style.display === "none") {
-            filtros.style.display = "block";
-            toggleFiltros.textContent = "Ocultar filtros";
-        } else {
-            filtros.style.display = "none";
-            toggleFiltros.textContent = "Mostrar filtros";
-        }
-    });
+    if (filtros.style.display === "none") {
+      filtros.style.display = "block";
+      toggleFiltros.textContent = "Ocultar filtros";
+    } else {
+      filtros.style.display = "none";
+      toggleFiltros.textContent = "Mostrar filtros";
+    }
+  });
 });
