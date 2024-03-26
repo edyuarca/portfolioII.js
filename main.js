768d280629f62bc19050ce16b4d2c7820946adbd
cinthia sosa <cinthia_ramlah@hotmail.com>
2024-03-25 22:03:57 -0300

    se modifica main.js

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 //FILTROS
 
-document.addEventListener("DOMContentLoaded", function() {
-    var toggleFiltros = document.getElementById("toggle-filtros");
-    var filtros = document.getElementById("filtros");
+document.addEventListener("DOMContentLoaded", function () {
+  let toggleFiltros = document.getElementById("toggle-filtros");
+  let filtros = document.getElementById("filtros");
 
-    toggleFiltros.addEventListener("click", function(event) {
-        event.preventDefault(); // Evita que el enlace recargue la página
+  toggleFiltros.addEventListener("click", function (event) {
+    event.preventDefault(); // Evita que el enlace recargue la página
 
-        // Verifica el estado actual de los filtros
-        if (filtros.style.display === "none") {
-            // Si están ocultos, muestra los filtros
-            filtros.style.display = "block";
-            toggleFiltros.textContent = "Ocultar filtros";
-        } else {
-            // Si están visibles, oculta los filtros
-            filtros.style.display = "none";
-            toggleFiltros.textContent = "Mostrar filtros";
-        }
-    });
+    // Verifica el estado actual de los filtros
+    if (filtros.style.display === "none") {
+      // Si están ocultos, muestra los filtros
+      filtros.style.display = "block";
+      toggleFiltros.textContent = "Ocultar filtros";
+    } else {
+      // Si están visibles, oculta los filtros
+      filtros.style.display = "none";
+      toggleFiltros.textContent = "Mostrar filtros";
+    }
+  });
 });
 
 //Contendor operaciones vacío a agregar nueva operación
